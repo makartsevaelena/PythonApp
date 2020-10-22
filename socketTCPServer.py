daa347ce7420a17d7983e623d409b2a69910a1b7
Elena <makartsevaea@gmail.com>
2020-10-22 18:14:03 +0300

    add tcp server/client add unix server/client

--- socketTCPServer.py
+++ socketTCPServer.py
@@ -1,13 +1,16 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('127.0.0.1', 8888))
+sock.listen(5)
 
 while True:
     try:
-        result = sock.recv(1024)
+        client, addr = sock.accept()
     except KeyboardInterrupt:
         sock.close()
         break
     else:
+        result = client.recv(1024)
+        client.close()
         print('Message', result.decode('utf-8'))
--- socketUnixServer.py
+++ socketUnixServer.py
@@ -1,7 +1,13 @@
+import os
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('127.0.0.1', 8888))
+unix_sock_name = 'unix.sock'
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+
+if os.path.exists(unix_sock_name):
+    os.remove(unix_sock_name)
+
+sock.bind(unix_sock_name)
 
 while True:
     try:
