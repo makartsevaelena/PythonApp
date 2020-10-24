4e93c338699aa3cc4a16820c0acb94a04dba2628
Elena <makartsevaea@gmail.com>
2020-10-24 11:53:55 +0300

    add new file

--- query_by_name.py
+++ query_by_name.py
@@ -1,22 +1,69 @@
-# coding=utf-8
-def check_query(query):
-    # напишите код тела функции
-    query_list = query.split(', ')
-    if query_list[0] == 'Анфиса':
-        text = 'запрос к Анфисе'
+DATABASE = {
+    'Сергей': 'Омск',
+    'Соня': 'Москва',
+    'Миша': 'Москва',
+    'Дима': 'Челябинск',
+    'Алина': 'Красноярск',
+    'Егор': 'Пермь',
+    'Коля': 'Красноярск'
+}
+
+
+def format_count_friends(count_friends):
+    if count_friends == 1:
+        return '1 друг'
+    elif 2 <= count_friends <= 4:
+        return f'{count_friends} друга'
     else:
-        text = 'запрос к кому-то ещё'
-    return text
+        return f'{count_friends} друзей'
 
 
-# дальше следует код, вызывающий вашу функцию; не изменяйте его:
-queries = [
-    'Анфиса, сколько у меня друзей?',
-    'Андрей, ну где ты был?',
-    'Андрей, ну обними меня скорей!',
-    'Анфиса, кто все мои друзья?'
-]
+def process_anfisa(query):
+    if query == 'сколько у меня друзей?':
+        count_string = format_count_friends(len(DATABASE))
+        return f'У тебя {count_string}'
+    elif query == 'кто все мои друзья?':
+        friends_string = ', '.join(DATABASE.keys())
+        return f'Твои друзья: {friends_string}'
+    elif query == 'где все мои друзья?':
+        unique_cities = set(DATABASE.values())
+        cities_string = ', '.join(unique_cities)
+        return f'Твои друзья в городах: {cities_string}'
+    else:
+        return '<неизвестный запрос>'
 
-for q in queries:
-    result = check_query(q)
-    print(q, '-', result)
+
+def process_query(query):
+    query_list = query.split(', ')
+    name = query_list[0]
+    query = query_list[1]
+    if name == 'Анфиса':
+        return process_anfisa(query)
+    else:
+        return process_friend(name, query)
+
+
+def process_friend(name, query):
+    if name not in DATABASE.keys():
+        return f'У тебя нет друга по имени {name}'
+    if query == 'ты где?':
+        return f'{name} в городе {DATABASE[name]}'
+    else:
+        return '<неизвестный запрос>'
+
+
+def runner():
+    queries = [
+        'Анфиса, сколько у меня друзей?',
+        'Анфиса, кто все мои друзья?',
+        'Анфиса, где все мои друзья?',
+        'Анфиса, кто виноват?',
+        'Коля, ты где?',
+        'Соня, что делать?',
+        'Антон, ты где?'
+    ]
+    for query in queries:
+        print(query, '-', process_query(query))
+
+
+runner()
